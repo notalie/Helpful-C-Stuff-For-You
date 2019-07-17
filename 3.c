b9ced59d2b5eb36be91f833bc5091f162b076977
zenzlia <42208300+zenzlia@users.noreply.github.com>
2019-07-17 21:48:21 +1000

    Added first C file

---
+++ 3.c
@@ -0,0 +1,48 @@
+/*
+                \||/
+                |  @___oo
+      /\  /\   / (__,,,,|
+     ) /^\) ^\/ _)
+     )   /^\/   _)
+     )   _ /  / _)
+ /\  )/\/ ||  | )_)
+<  >      |(,,) )__)
+ ||      /    \)___)\
+ | \____(      )___) )___
+  \______(_______;;; __;;;
+"I'm so tired"
+The purpose of this code is to show how pointers work and how they can 
+be used to change variables without the need to return them
+*/
+
+#include <stdio.h>
+
+//Swaps the two values using pass by copy/value
+void swap1(int a, int b) {
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+
+//Swaps the two values using pass by reference/pointer
+void swap2(int* a, int* b) {
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int a = 1;
+    int b = 2;
+    printf("Initial Values:\t A: %d\t B: %d\n", a, b);
+    swap1(a, b);
+    //Notice how how they don't change after being swapped even though swap1 
+    //should swap them
+    printf("After Swap 1:\t A: %d\t B: %d\n", a, b);
+    swap2(&a, &b);
+    //After this swap the numbers will change because we passed in a pointer 
+    //to the values
+    printf("After Swap 2:\t A: %d\t B: %d\n", a, b);
+}
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Helpful C Stuff For You xx
  NO ONE IS FAILING ON MY WATCH 🔥🔥🔥🔥🔥
+**Please do not copy any of this code for your assignments this is just really simple stuff and 100% chance someone has already copied it.
+This code exists for the sole purpose of demonstrating C's functionality. I am not putting any of this code on my assignments either.**
+To the dishonest motherfucker who decides to copy all the code from this I take no responsibility in your academic misconduct cases goodluck.
+
+**TABLE OF CONTENTS (not in any specific order btw)**
+1. 
+2. 
+3.c Pointer Demonstration, basic swap function
+4. 
+5. 
+6. 
+7. 
+8. 
+9. 
+10. 
