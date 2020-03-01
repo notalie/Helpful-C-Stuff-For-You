9c9d77b7f1c6f4636b1c648bbfe54b4c393fe2ea
Hamish Bultitude <sendanemailtohamish@gmail.com>
2020-03-01 21:11:28 +1000

    Added struct info

--- 4.c
+++ 4.c
@@ -8,6 +8,7 @@
     This file is an introduction to S T R U C T S
 */
 #include <stdio.h>
+#include <stdlib.h>
 
 /* Struct????
     A struct is a data type that contains different types inside of it.
@@ -19,4 +20,17 @@ int main(int argc, char** argv) {
         char* name;
         int age;
     };
+
+    //We now want to create the struct, we do this via big scary MALLOC
+    //stdlib.h is required in order to call malloc! Avoid warnings this way.
+    //Creating a struct is just like instantiating an object in python or java, it is now on the heap
+    struct Person* p = malloc(sizeof(struct Person));
+    
+    //now we can edit the associated values of the struct!
+    p->age = 4;
+    char* name = "Hamish";
+    p->name = name;
+
+    printf("%d, %s\n", p->age, p->name);
+
 }
